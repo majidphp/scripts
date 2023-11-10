c715d9f4082f2c6e7341d25aa4510afa80e02d12
Majid <31689325+majidphp@users.noreply.github.com>
2023-11-09 21:25:47 -0700

    Update certbot_install.sh

--- certbot_install.sh
+++ certbot_install.sh
@@ -5,3 +5,8 @@ python3 -m venv /opt/certbot/
 /opt/certbot/bin/pip install certbot certbot-apache
 # /opt/certbot/bin/pip install certbot certbot-nginx
 ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+
+DOMAIN=$1
+EMAIL=$2
+
+certbot certonly -d $DOMAIN --non-interactive --agree-tos -m $EMAIL
