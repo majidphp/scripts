78329b0efb6513ebd52329453902439cb8b21703
Majid <31689325+majidphp@users.noreply.github.com>
2023-11-09 23:01:33 -0700

    Update certbot_install.sh

--- certbot_install.sh
+++ certbot_install.sh
@@ -14,14 +14,41 @@ then
         exit
 fi
 
+apt update
+
+PS3="Please select your webserver: "
+options=("Apache" "Nginx")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Apache")
+            WEBSRV='apache'
+            WEBSERVICE='apache2'
+            break
+            ;;
+        "Nginx")
+            WEBSRV='nginx'
+            WEBSERVICE='nginx'
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+servstat=$(service $WEBSERVICE status)
+
+if [[ $servstat == *"is not running"* ]]; then
+        echo "Please make sure $WEBSRV is running"
+        exit
+fi
+
 apt install python3 python3-venv libaugeas0 -y
 python3 -m venv /opt/certbot/
 /opt/certbot/bin/pip install --upgrade pip
-/opt/certbot/bin/pip install certbot certbot-apache
- /opt/certbot/bin/pip install certbot certbot-nginx
+/opt/certbot/bin/pip install certbot certbot-$WEBSRV
 ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 
 DOMAIN=$1
 EMAIL=$2
 
-certbot certonly -d $DOMAIN --non-interactive --agree-tos -m $EMAIL
+certbot certonly -d $DOMAIN --$WEBSRV --non-interactive --agree-tos -m $EMAIL
